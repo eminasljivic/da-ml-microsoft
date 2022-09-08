0e3e77bbb45bb1a88dce658838cf5710fc1ac3bc
polleichtnermoritz <moritz.polleichtner@outlook.com>
2022-09-08 20:56:46 +0200

    add endpoints in frontend for handling metrics

--- frontend-da/src/app/comparison/comparison.component.ts
+++ frontend-da/src/app/comparison/comparison.component.ts
@@ -2,7 +2,10 @@ import {Component, ViewChild} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {NgxMatFileInputComponent} from "@angular-material-components/file-input/lib/file-input.component";
 import {HttpClient} from "@angular/common/http";
+import {BoundingBox, BoundingBoxState} from "../model/bounding-box";
 import {DataService} from "../core/service/data.service";
+import { DatePipe } from '@angular/common';
+import { Metric, type } from '../model/metric';
 
 @Component({
   selector: 'app-comparison',
@@ -12,25 +15,58 @@ import {DataService} from "../core/service/data.service";
 export class ComparisonComponent{
 
   @ViewChild("file") file!: NgxMatFileInputComponent;
+  myCanvas!: HTMLCanvasElement;
+  @ViewChild('pdfViewer') pdfViewer!: any;
+
   filePath: FormControl = new FormControl();
   url: string = "";
+  img = new Image();
+  boundingBoxes: BoundingBox[] = [];
+  factor: number = 1;
+  canvasContext!: CanvasRenderingContext2D;
+  data: any
+  t0: number = 0;
+  t1: number = 0;
+  pdfsrc: any
 
   constructor(private http: HttpClient, private dataService: DataService)
   {}
 
   getInvoice() {
     this.url = URL.createObjectURL(this.filePath.value);
-    console.log(this.url)
-  }
+    console.log(this.filePath)
+    }
 
   send(){
     console.log("begin")
+
     this.dataService.loading = true;
     this.http.get(this.url, {responseType: 'blob'}).subscribe((resp: any) => {
-      this.http.post("http://localhost:7019/api/Function1", resp).subscribe((size: any) => {
-          console.log(resp)
+      this.t0 = Date.now()
+      this.http.post("http://localhost:7019/api/getInvoiceDetails", resp).subscribe((invoice: any) => {
+        this.data = invoice;
+        this.t1 = Date.now()
+
+        var req:any = {
+          begin:this.t0,
+          end: this.t1,
+          duration: this.t1 - this.t0,
+          type: "FORMRECOGNIZER",
+          invoice: this.data
+        }
+        this.http.post("http://localhost:7019/api/SaveMetrics", req).subscribe((metric: any) => {
+          console.log(metric);
           this.dataService.loading = false;
         });
       });
-    }
+    });
+  }
+
+  test(){
+    this.dataService.loading = true;
+    this.http.post("http://localhost:7019/api/GetMetrics", null).subscribe((metrics: any) => {
+          console.log(metrics);
+          this.dataService.loading = false;
+        });
+  }
 }
